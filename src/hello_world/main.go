fc0e419975cf94c17bbebb46f92d236a1d9d8c61
Jhon Luengas <jeluengasm@unal.edu.co>
2022-08-11 21:59:05 -0500

    For Loops

--- src/hello_world/main.go
+++ src/hello_world/main.go
@@ -2,25 +2,26 @@ package main
 
 import "fmt"
 
-func getMessage(s1 string, s2 string) {
-	fmt.Println(s1, s2)
-}
-
-func duplicateNumber(n int32) int32 {
-	return n * 2
-}
-
-func doubleReturn(a int) (b, c int) {
-	return a, a * 2
-}
-
 func main() {
-	getMessage("Hello,", "world!")
-	val1 := duplicateNumber(4)
-	fmt.Println(val1)
-	val2, val3 := doubleReturn(4)
-	fmt.Println(val2, val3)
+	// For with conditional
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
 
-	val4, _ := doubleReturn(4) // if one of the values wont need it
-	fmt.Println("Val4:", val4)
+	fmt.Println()
+
+	// for while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	// for forever
+
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 }
