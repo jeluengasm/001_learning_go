2b22793f8545c8e1c5fd4fbd4960820aa30b146c
Jhon Luengas <jeluengasm@unal.edu.co>
2022-08-11 23:27:33 -0500

    Strings handling

--- src/hello_world/main.go
+++ src/hello_world/main.go
@@ -1,30 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindrome(text string) bool {
+	text = strings.ToLower(text)
+	var textReversed string
+
+	for i := len(text) - 1; i >= 0; i-- {
+		textReversed += string(text[i])
+	}
+
+	if textReversed == text {
+		return true
+	} else {
+		return false
+	}
+}
 
 func main() {
-	// Arrays
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
+	slice := []string{"Hello", "World", "from", "Golang"}
 
-	// Slice
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
+	for key, val := range slice {
+		fmt.Println(key, val)
+	}
 
-	// Slicing
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
+	fmt.Println()
 
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
+	for _, val := range slice {
+		fmt.Println(val)
+	}
 
-	// Append another slice
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
+	fmt.Println()
+
+	for key := range slice {
+		fmt.Println(key)
+	}
+
+	fmt.Println("Ama:", isPalindrome("Ama"))
+	fmt.Println("taco cat:", isPalindrome("taco cat"))
 }
